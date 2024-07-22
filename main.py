4ca3d12158d459fc912bfcac596a088180c379cc
h0lylag <h0lylag@gravemind.sh>
2024-07-22 01:12:02 -0700

    borders and active client highlighting

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import subprocess
 from datetime import datetime
 from PyQt5.QtWidgets import (
     QApplication, QMainWindow, QSystemTrayIcon, QMenu, QAction, QVBoxLayout,
-    QCheckBox, QLabel, QWidget, QTabWidget
+    QCheckBox, QLabel, QWidget, QTabWidget, QLineEdit, QHBoxLayout
 )
 from PyQt5.QtGui import QIcon, QPixmap, QImage, QPainter, QColor, QFont
 from PyQt5.QtCore import Qt, QPoint, QThread, QTimer, pyqtSignal
@@ -24,7 +24,10 @@ def load_config():
         "settings": {
             "thumbnail_scaling": 7.5,
             "thumbnail_opacity": 100,
-            "application_position": [100, 100]
+            "application_position": [100, 100],
+            "enable_borders": True,
+            "active_border_color": "#47f73e",
+            "inactive_border_color": "#808080"
         },
         "thumbnail_position": {}
     }
@@ -109,20 +112,21 @@ class UpdateThread(QThread):
 class WindowPreview(QWidget):
     SNAP_DISTANCE = 20
 
-    def __init__(self, x11_interface, window_id, window_title, previews, config):
+    def __init__(self, x11_interface, window_id, window_title, previews, config, manager):
         super().__init__()
         self.window_id = window_id
         self.window_title = window_title
         self.x11_interface = x11_interface
         self.previews = previews
         self.config = config
+        self.manager = manager
         self.label = QLabel(self)
 
         layout = QVBoxLayout()
         layout.addWidget(self.label)
         layout.setContentsMargins(0, 0, 0, 0)  # Remove any layout margins
         self.setLayout(layout)
-        self.setWindowFlags(Qt.Widget | Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)
+        self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)
         self.setAttribute(Qt.WA_TranslucentBackground)
         self.setContentsMargins(0, 0, 0, 0)  # Remove widget margins
         self.setWindowOpacity(config["settings"]["thumbnail_opacity"] / 100)
@@ -137,6 +141,7 @@ class WindowPreview(QWidget):
         self.update_thread.start()
 
         self.load_position()
+        self.update_border()
 
     def set_pixmap(self, pixmap, new_width, new_height):
         self.label.setPixmap(pixmap)
@@ -149,6 +154,7 @@ class WindowPreview(QWidget):
     def mousePressEvent(self, event):
         if event.button() == Qt.LeftButton:
             self.x11_interface.focus_and_raise_window(self.window_id)
+            self.manager.set_active_preview(self)
         elif event.button() == Qt.RightButton:
             self.dragging = True
             self.drag_position = event.globalPos() - self.frameGeometry().topLeft()
@@ -213,6 +219,16 @@ class WindowPreview(QWidget):
             return self.window_title.split(" - ")[-1]
         return "Unknown"
 
+    def update_border(self):
+        if self.config["settings"]["enable_borders"]:
+            border_color = self.config["settings"]["inactive_border_color"]
+            if self.manager.active_preview == self:
+                border_color = self.config["settings"]["active_border_color"]
+            self.setStyleSheet(f"QWidget {{ border: 3px solid {border_color}; }}")
+        else:
+            self.setStyleSheet("QWidget { border: none; }")
+
+
 class WindowManager:
     def __init__(self, x11_interface, config):
         self.x11_interface = x11_interface
@@ -221,6 +237,7 @@ class WindowManager:
         self.timer = QTimer()
         self.timer.timeout.connect(self.update_previews)
         self.timer.start(1000)
+        self.active_preview = None
 
     def update_previews(self):
         window_list = self.x11_interface.list_windows()
@@ -231,7 +248,7 @@ class WindowManager:
         closed_windows = [preview for preview in self.previews if preview.window_id not in {window_id for window_id, _ in eve_windows}]
 
         for window_id, window_title in new_windows:
-            preview = WindowPreview(self.x11_interface, window_id, window_title, self.previews, self.config)
+            preview = WindowPreview(self.x11_interface, window_id, window_title, self.previews, self.config, self)
             preview.show()
             self.previews.append(preview)
 
@@ -239,139 +256,187 @@ class WindowManager:
             self.previews.remove(preview)
             preview.close()
 
+        for preview in self.previews:
+            preview.update_border()
+
+    def set_active_preview(self, preview):
+        if self.active_preview:
+            self.active_preview.update_border()
+        self.active_preview = preview
+        self.active_preview.update_border()
+        self.update_all_borders()
+
+    def update_all_borders(self):
+        for preview in self.previews:
+            preview.update_border()
+
+
+class ThumbnailsTab(QWidget):
+    def __init__(self, parent=None):
+        super(ThumbnailsTab, self).__init__(parent)
+        layout = QVBoxLayout()
+
+        self.enable_borders_checkbox = QCheckBox("Enable Borders")
+        self.enable_borders_checkbox.setChecked(config["settings"]["enable_borders"])
+        self.enable_borders_checkbox.stateChanged.connect(self.toggle_borders)
+
+        self.active_border_color_label = QLabel("Active Client Border Color:")
+        self.active_border_color_input = QLineEdit(config["settings"]["active_border_color"])
+
+        self.inactive_border_color_label = QLabel("Inactive Client Border Color:")
+        self.inactive_border_color_input = QLineEdit(config["settings"]["inactive_border_color"])
+
+        layout.addWidget(self.enable_borders_checkbox)
+        layout.addWidget(self.active_border_color_label)
+        layout.addWidget(self.active_border_color_input)
+        layout.addWidget(self.inactive_border_color_label)
+        layout.addWidget(self.inactive_border_color_input)
+
+        self.setLayout(layout)
+
+        self.active_border_color_input.editingFinished.connect(self.update_active_border_color)
+        self.inactive_border_color_input.editingFinished.connect(self.update_inactive_border_color)
+
+    def toggle_borders(self, state):
+        config["settings"]["enable_borders"] = bool(state)
+        save_config(config)
+
+    def update_active_border_color(self):
+        color = self.active_border_color_input.text()
+        if self.is_valid_hex_color(color):
+            config["settings"]["active_border_color"] = color
+            save_config(config)
+
+    def update_inactive_border_color(self):
+        color = self.inactive_border_color_input.text()
+        if self.is_valid_hex_color(color):
+            config["settings"]["inactive_border_color"] = color
+            save_config(config)
+
+    def is_valid_hex_color(self, color):
+        if color.startswith('#') and len(color) == 7:
+            try:
+                int(color[1:], 16)
+                return True
+            except ValueError:
+                pass
+        return False
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.setWindowTitle("EVE-L Preview")
+        self.setGeometry(100, 100, 400, 300)
+
+        self.tray_icon = QSystemTrayIcon(self)
+        self.tray_icon.setIcon(QIcon("icon.png"))
+        self.tray_icon.setToolTip("EVE-L Preview")
+
+        show_action = QAction("Show", self)
+        quit_action = QAction("Exit", self)
+        show_action.triggered.connect(self.show)
+        quit_action.triggered.connect(QApplication.instance().quit)
+
+        tray_menu = QMenu()
+        tray_menu.addAction(show_action)
+        tray_menu.addAction(quit_action)
+
+        self.tray_icon.setContextMenu(tray_menu)
+        self.tray_icon.activated.connect(self.on_tray_icon_activated)
+
+        self.tray_icon.show()
+
+        self.init_ui()
+        self.load_position()
+
+    def init_ui(self):
+        self.tabs = QTabWidget()
+
+        self.general_tab = GeneralTab()
+        self.settings_tab = SettingsTab()
+        self.thumbnails_tab = ThumbnailsTab()
+        self.profiles_tab = ProfilesTab()
+
+        self.tabs.addTab(self.general_tab, "General")
+        self.tabs.addTab(self.settings_tab, "Settings")
+        self.tabs.addTab(self.thumbnails_tab, "Thumbnails")
+        self.tabs.addTab(self.profiles_tab, "Profiles")
+
+        self.setCentralWidget(self.tabs)
+
+    def closeEvent(self, event):
+        self.save_position()
+        if self.isVisible():
+            event.ignore()
+            self.hide()
+
+    def on_tray_icon_activated(self, reason):
+        if reason == QSystemTrayIcon.DoubleClick:
+            self.show()
+            self.raise_()
+            self.activateWindow()
+
+    def load_position(self):
+        pos = config["settings"].get("application_position", [100, 100])
+        self.move(pos[0], pos[1])
+
+    def save_position(self):
+        config["settings"]["application_position"] = [self.x(), self.y()]
+        save_config(config)
+
 class GeneralTab(QWidget):
     def __init__(self, parent=None):
         super(GeneralTab, self).__init__(parent)
         layout = QVBoxLayout()
-        
+
         self.checkbox1 = QCheckBox("General option 1")
         self.checkbox2 = QCheckBox("General option 2")
-        
+
         layout.addWidget(self.checkbox1)
         layout.addWidget(self.checkbox2)
-        
+
         self.setLayout(layout)
 
 class SettingsTab(QWidget):
     def __init__(self, parent=None):
         super(SettingsTab, self).__init__(parent)
         layout = QVBoxLayout()
-        
+
         self.checkbox1 = QCheckBox("Settings option 1")
         self.checkbox2 = QCheckBox("Settings option 2")
-        
-        layout.addWidget(self.checkbox1)
-        layout.addWidget(self.checkbox2)
-        
-        self.setLayout(layout)
 
-class ThumbnailsTab(QWidget):
-    def __init__(self, parent=None):
-        super(ThumbnailsTab, self).__init__(parent)
-        layout = QVBoxLayout()
-        
-        self.checkbox1 = QCheckBox("Thumbnails option 1")
-        self.checkbox2 = QCheckBox("Thumbnails option 2")
-        
         layout.addWidget(self.checkbox1)
         layout.addWidget(self.checkbox2)
-        
+
         self.setLayout(layout)
 
 class ProfilesTab(QWidget):
     def __init__(self, parent=None):
         super(ProfilesTab, self).__init__(parent)
         layout = QVBoxLayout()
-        
+
         self.checkbox1 = QCheckBox("Profiles option 1")
         self.checkbox2 = QCheckBox("Profiles option 2")
-        
+
         layout.addWidget(self.checkbox1)
         layout.addWidget(self.checkbox2)
-        
+
         self.setLayout(layout)
-
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super(MainWindow, self).__init__()
-        
-        self.setWindowTitle("EVE-L Preview")
-        self.setGeometry(100, 100, 400, 300)
-        
-        self.tray_icon = QSystemTrayIcon(self)
-        self.tray_icon.setIcon(QIcon("icon.png"))
-        
-        show_action = QAction("Show", self)
-        quit_action = QAction("Exit", self)
-        show_action.triggered.connect(self.show)
-        quit_action.triggered.connect(QApplication.instance().quit)
-        
-        tray_menu = QMenu()
-        tray_menu.addAction(show_action)
-        tray_menu.addAction(quit_action)
-        
-        self.tray_icon.setContextMenu(tray_menu)
-        self.tray_icon.activated.connect(self.on_tray_icon_activated)
-        
-        self.tray_icon.show()
-        
-        self.init_ui()
-        self.load_position()
-
-    def init_ui(self):
-        self.tabs = QTabWidget()
-        
-        self.general_tab = GeneralTab()
-        self.settings_tab = SettingsTab()
-        self.thumbnails_tab = ThumbnailsTab()
-        self.profiles_tab = ProfilesTab()
-        
-        self.tabs.addTab(self.general_tab, "General")
-        self.tabs.addTab(self.settings_tab, "Settings")
-        self.tabs.addTab(self.thumbnails_tab, "Thumbnails")
-        self.tabs.addTab(self.profiles_tab, "Profiles")
-        
-        self.setCentralWidget(self.tabs)
-    
-    def closeEvent(self, event):
-        self.save_position()
-        if self.isVisible():
-            event.ignore()
-            self.hide()
-            #self.tray_icon.showMessage(
-            #    "Tray Application",
-            #    "Application was minimized to tray.",
-            #    QSystemTrayIcon.Information,
-            #    2000
-            #)
-    
-    def on_tray_icon_activated(self, reason):
-        if reason == QSystemTrayIcon.DoubleClick:
-            self.show()
-            self.raise_()
-            self.activateWindow()
-    
-    def load_position(self):
-        pos = config["settings"].get("application_position", [100, 100])
-        self.move(pos[0], pos[1])
-    
-    def save_position(self):
-        config["settings"]["application_position"] = [self.x(), self.y()]
-        save_config(config)
 
 def main():
     global config
     app = QApplication(sys.argv)
     app.setQuitOnLastWindowClosed(False)
-    
+
     # Load config
     config = load_config()
-    
+
     # Create main window
     window = MainWindow()
     window_manager = WindowManager(X11Interface(), config)
     window.show()
-    
+
     sys.exit(app.exec_())
 
 if __name__ == '__main__':
