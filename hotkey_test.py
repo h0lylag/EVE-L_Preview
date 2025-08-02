2f6a154bb90f31461eaef466e8bf72df5d958156
h0ly lag <h0lylag@gravemind.sh>
2025-08-02 09:50:26 -0700

    shuffle old files around. dont want to delete them yet

--- hotkey_test.py
+++
@@ -1,9 +0,0 @@
-# sudo python3 listener.py
-import keyboard                 # pip install keyboard
-
-def on_key(e):
-    if e.event_type == 'down':  # key down only
-        print(e.name, e.scan_code)
-
-keyboard.hook(on_key)           # attach a global listener
-keyboard.wait()                 # block forever
